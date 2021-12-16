a3e8c94705aa04c66ff653406a583d3193366f79
Rinat Khusnutdinov <xusnutdinov.rin@gmail.com>
2021-12-16 21:41:34 +0300

    change JS file

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-let selectCurrent = document.querySelector('.select__current');
-const selectBody = document.querySelector('.select__body');
-const selectItems = document.querySelectorAll('.select__input');
+let selectCurrent = document.querySelector(".select__current");
+const selectBody = document.querySelector(".select__body");
+const selectItems = document.querySelectorAll(".select__input");
 
-selectCurrent.addEventListener('click', (event) => {
-	selectBody.classList.toggle('active');
+selectCurrent.addEventListener("click", (event) => {
+    selectBody.classList.toggle("active");
 });
 
 selectItems.forEach((item) => {
-	item.addEventListener('click', (event) => {
-		event.currentTarget.parentElement.classList.toggle('active');
-		let text = event.currentTarget.innerText;
-		selectCurrent.innerText = text;
-	});
-})
+    item.addEventListener("click", (event) => {
+        event.currentTarget.parentElement.classList.toggle("active");
+        let text = event.currentTarget.innerText;
+        selectCurrent.innerText = text;
+    });
+});
+
+console.log("Hello");
